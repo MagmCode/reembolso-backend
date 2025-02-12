5132bb9489c0c5ac3265211d6cb267eeae2f380a
MagmCode <mariaaguedez.08@gmail.com>
2025-02-12 17:29:21 -0400

    Update settings.py

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -111,7 +112,7 @@ WSGI_APPLICATION = 'Proyecto.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Usa os.path.join
     }
 }
 
