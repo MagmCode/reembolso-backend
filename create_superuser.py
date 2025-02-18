b97640b9707835c20517c5a265116a62b353128a
MagmCode <mariaaguedez.08@gmail.com>
2025-02-18 16:10:16 -0400

    Update create_superuser.py
    creacion de nuevo usuario

--- create_superuser.py
+++ create_superuser.py
@@ -5,12 +5,14 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto.settings')
 django.setup()
 
-from Apps.Aplicacion.models import Usuario
+# from Apps.Aplicacion.models import Usuario
+from django.contrib.auth import get_user_model
+Usuario = get_user_model()
 
 # Verifica si el usuario ya existe
 if not Usuario.objects.filter(username='123456').exists():
     # Crea un usuario con el campo 'username' (que almacena la cédula)
-    user = Usuario.objects.create_user(
+    admin = Usuario.objects.create_user(
         username='123456',  # Usar 'username' en lugar de 'cedula'
         password='Admin25',  # Contraseña del usuario
         email='admin@example.com',  # Correo electrónico (requerido por REQUIRED_FIELDS)
@@ -18,9 +20,37 @@ if not Usuario.objects.filter(username='123456').exists():
         last_name='User',  # Apellido (requerido por REQUIRED_FIELDS)
         fecha_nacimiento='1990-01-01'  # Fecha de nacimiento (requerido por REQUIRED_FIELDS)
     )
-    user.is_staff = True  # Hacer que el usuario sea parte del staff
-    user.is_superuser = True  # Hacer que el usuario sea un superusuario
-    user.save()
+    admin.is_staff = True  # Hacer que el usuario sea parte del staff
+    admin.is_superuser = True  # Hacer que el usuario sea un superusuario
+    admin.save()
     print("Usuario creado exitosamente.")
 else:
     print("El usuario ya existe.")
+
+USERS = [
+    {
+        'username': '29850418',
+        'password': 'Admin2503',
+        'email': 'maria@gmail.com',
+        'first_name': 'Maria',
+        'last_name': 'Mendoza',
+        'fecha_nacimiento': '2003-07-08',
+    },
+]
+
+for usuario_data in USERS:
+    if not Usuario.objects.filter(username=usuario_data['username']).exists():
+        usuario = Usuario.objects.create_user(
+            username=usuario_data['username'],
+            password=usuario_data['password'],
+            email=usuario_data['email'],
+            first_name=usuario_data['first_name'],
+            last_name=usuario_data['last_name'],
+            fecha_nacimiento=['fecha_nacimiento'],
+            is_staff=False,
+            is_superuser=False,
+        )
+        usuario.save()
+        print(f"Usuario {usuario.username} creado exitosamente.")
+    else:
+        print(f"El usuario {usuario_data['username']} ya existe.")
